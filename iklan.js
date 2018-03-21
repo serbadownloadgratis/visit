1bc20c908bcfcf721aa966508360437d2446918d
serbadownloadgratis <37610958+serbadownloadgratis@users.noreply.github.com>
2018-03-21 14:37:39 +0700

    Update iklan.js

--- iklan.js
+++ iklan.js
@@ -38,7 +38,7 @@ j++;}
 
 if(a_to_vh==false)
 
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://serba-downloadgratis.blogspot.com/2018/03/visit.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="http://serba-downloadgratis.blogspot.com/p/generate_48.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 
