72a9c62991f4c01c5e43ea654ce90f2b9c260199
serbadownloadgratis <37610958+serbadownloadgratis@users.noreply.github.com>
2018-03-23 10:36:14 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -19,7 +19,7 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*3)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/03/cara-membuat-email-gmail.html"}
 if(x=="2"){xxx="/2018/03/cara-mengatasi-error-404.html"}
